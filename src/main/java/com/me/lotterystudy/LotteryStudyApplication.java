73752daf0008a143004115a22d39e1e30f8f2939
liulc <liulc@idss-cn.com>
2019-07-13 10:00:08 +0800

    init

---
+++ src/main/java/com/me/lotterystudy/LotteryStudyApplication.java
@@ -0,0 +1,13 @@
+package com.me.lotterystudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotteryStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LotteryStudyApplication.class, args);
+    }
+
+}
